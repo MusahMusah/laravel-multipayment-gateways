47405506d9fb9d92e0d5695b7ececca44f4e9716
MusahMusah <shehialiyu0@gmail.com>
2023-01-09 11:29:55 +0100

    add payment api implementation for stripe gateway

--- src/Gateways/StripeService.php
+++ src/Gateways/StripeService.php
@@ -2,6 +2,119 @@
 
 namespace MusahMusah\LaravelMultipaymentGateways\Gateways;
 
+use GuzzleHttp\Exception\GuzzleException;
+use MusahMusah\LaravelMultipaymentGateways\Exceptions\HttpMethodFoundException;
+use MusahMusah\LaravelMultipaymentGateways\Exceptions\InvalidConfigurationException;
+use MusahMusah\LaravelMultipaymentGateways\Traits\ConsumesExternalServices;
+
 class StripeService
 {
+    use ConsumesExternalServices;
+
+    /**
+     * The base uri to consume the Stripe's service
+     *
+     * @var string
+     */
+    protected $baseUri;
+
+    /**
+     * The secret to consume the Stripe's service
+     *
+     * @var string
+     */
+    protected $secret;
+
+    /**
+     * The plans to consume the Stripe's service
+     *
+     * @var array
+     */
+    protected $plans;
+
+    public function __construct()
+    {
+        $this->baseUri = config('multipayment-gateways.stripe.base_uri');
+        $this->secret = config('multipayment-gateways.stripe.secret');
+        $this->plans = config('multipayment-gateways.stripe.plans');
+    }
+
+    /**
+     * Resolve the authorization URL / Endpoint
+     *
+     * @param $queryParams
+     * @param $formParams
+     * @param $headers
+     * @return void
+     */
+    public function resolveAuthorization(&$queryParams, &$formParams, &$headers): void
+    {
+        $headers['Authorization'] = $this->resolveAccessToken();
+    }
+
+    /**
+     * Set the access token for the request
+     *
+     * @return string
+     */
+    public function resolveAccessToken(): string
+    {
+        return "Bearer {$this->secret}";
+    }
+
+    /**
+     * Decode the response
+     *
+     * @return mixed
+     */
+    public function decodeResponse(): mixed
+    {
+        return json_decode($this->response, true);
+    }
+
+    /**
+     * Create a new payment intent
+     * @param int $value
+     * @param string|null $paymentMethod
+     * @param string $confirmationMethod
+     * @param bool $confirm
+     * @return array
+     * @throws GuzzleException
+     * @throws HttpMethodFoundException
+     * @throws InvalidConfigurationException
+     */
+    public function createIntent(int $value, string $paymentMethod = null, string $confirmationMethod = 'automatic', bool $confirm = true): array
+    {
+        return $this->makeRequest(
+            'POST',
+            '/v1/payment_intents',
+            [],
+            [
+                'amount' => $value,
+                'currency' => config('multipayment-gateways.stripe.currency'),
+                'payment_method_types' => ['card'],
+                'payment_method' => $paymentMethod,
+                'confirmation_method' => $confirmationMethod,
+                'confirm' => $confirm,
+            ],
+            [],
+            $isJsonRequest = true
+        );
+    }
+
+    /**
+     * Confirm a payment intent
+     * @param string $paymentIntentId
+     * @return array
+     * @throws GuzzleException
+     * @throws HttpMethodFoundException
+     * @throws InvalidConfigurationException
+     */
+    public function confirmIntent(string $paymentIntentId): array
+    {
+        return $this->makeRequest(
+            'POST',
+            "/v1/payment_intents/{$paymentIntentId}/confirm",
+        );
+    }
 }
