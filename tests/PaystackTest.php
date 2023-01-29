be641718b8a9e1e127f2ad0924b9969b9f85a013
MusahMusah <shehialiyu0@gmail.com>
2023-01-29 11:17:17 +0100

    add test for paystack checkout redirect

--- tests/PaystackTest.php
+++ tests/PaystackTest.php
@@ -1,9 +1,68 @@
 <?php
 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Http;
 use MusahMusah\LaravelMultipaymentGateways\Contracts\PaystackContract;
 
 beforeEach(function () {
     $this->paystack = $this->mock(PaystackContract::class);
+});
+
+it('can redirect to checkout for payment using passed arguments', function () {
+    $this->paystack
+        ->shouldReceive('redirectToCheckout')
+        ->once()
+        ->with([
+            'amount' => 1000,
+            'email' => 'musahmusah@test.com',
+            'reference' => '123456789',
+            'callback_url' => 'https://example.com',
+        ])
+        ->andReturnValues([
+            new RedirectResponse('https://checkout.paystack.com/123456789'),
+            [
+                'status' => true,
+                'message' => 'Checkout URL generated',
+                'data' => [
+                    'authorization_url' => 'https://checkout.paystack.com/123456789',
+                    'access_code' => '123456789',
+                    'reference' => '123456789',
+                ],
+            ],
+        ])
+        ->andReturn(new RedirectResponse('https://checkout.paystack.com/123456789'));
+
+    expect($this->paystack->redirectToCheckout([
+        'amount' => 1000,
+        'email' => 'musahmusah@test.com',
+        'reference' => '123456789',
+        'callback_url' => 'https://example.com',
+    ]))
+    ->toBeObject()
+    ->toBeInstanceOf(RedirectResponse::class);
+});
+
+it('can redirect to checkout for payment using request() arguments', function () {
+    $this->paystack
+        ->shouldReceive('redirectToCheckout')
+        ->once()
+        ->andReturnValues([
+            new RedirectResponse('https://checkout.paystack.com/123456789'),
+            [
+                'status' => true,
+                'message' => 'Checkout URL generated',
+                'data' => [
+                    'authorization_url' => 'https://checkout.paystack.com/123456789',
+                    'access_code' => '123456789',
+                    'reference' => '123456789',
+                ],
+            ],
+        ])
+        ->andReturn(new RedirectResponse('https://checkout.paystack.com/123456789'));
+
+    expect($this->paystack->redirectToCheckout())
+        ->toBeObject()
+        ->toBeInstanceOf(RedirectResponse::class);
 });
 
 it('can get the list of all banks', function () {
@@ -13,7 +72,7 @@ it('can get the list of all banks', function () {
         ->andReturn([
             'status' => true,
             'message' => 'Banks retrieved',
-            'data' => '*',
+            'data' => ['*'],
         ]);
 
     expect($this->paystack->getBanks())
@@ -21,6 +80,17 @@ it('can get the list of all banks', function () {
         ->toBe([
             'status' => true,
             'message' => 'Banks retrieved',
-            'data' => '*',
+            'data' => ['*'],
         ]);
+});
+
+it('can make fake http request to get list of banks', function () {
+    $body = file_get_contents(__DIR__.'/Fixtures/banks.json');
+
+    Http::fake([
+        'https://api.paystack.co/bank' => Http::response($body, 200),
+    ]);
+
+    expect(json_decode($body, true))
+        ->toBeArray();
 });
