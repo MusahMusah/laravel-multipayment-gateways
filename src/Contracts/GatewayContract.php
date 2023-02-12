a4e5ad4da71d1564abe81bfcada1e239813f9538
MusahMusah <MusahMusah@users.noreply.github.com>
2023-02-12 16:58:46 +0000

    Fix styling

--- src/Contracts/GatewayContract.php
+++ src/Contracts/GatewayContract.php
@@ -5,11 +5,18 @@ namespace MusahMusah\LaravelMultipaymentGateways\Contracts;
 interface GatewayContract
 {
     public function setPaymentGateway(): void;
+
     public function setBaseUri(): void;
+
     public function setSecret(): void;
+
     public function resolveAuthorization(&$queryParams, &$formParams, &$headers): void;
+
     public function resolveAccessToken(): string;
+
     public function decodeResponse(): array;
+
     public function getResponse(): array;
+
     public function getData(): array;
 }
