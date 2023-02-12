fc49f1ffef428ee35494bc860f7517592305908a
MusahMusah <shehialiyu0@gmail.com>
2023-02-12 17:11:17 +0100

    interfaces for methods in abstract class

---
+++ src/Contracts/GatewayContract.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MusahMusah\LaravelMultipaymentGateways\Contracts;
+
+interface GatewayContract
+{
+    public function setPaymentGateway(): void;
+    public function setBaseUri(): void;
+    public function setSecret(): void;
+    public function resolveAuthorization(&$queryParams, &$formParams, &$headers): void;
+    public function resolveAccessToken(): string;
+    public function decodeResponse(): array;
+    public function getResponse(): array;
+    public function getData(): array;
+}
