67c0a85911e3b0d41b53073e335d8a373cd04558
MusahMusah <shehialiyu0@gmail.com>
2023-03-15 09:47:28 +0100

    added flutterwave webhook config

--- config/multipayment-gateways.php
+++ config/multipayment-gateways.php
@@ -108,7 +108,56 @@ return [
              *
              * Default: 'job'
              */
-            'payment_webhook_handler' => 'event',
+            'payment_webhook_handler' => 'job',
+
+            /**
+             * The payment_webhook_job option allows you to specify the job class that will be used to process webhook requests for payment methods.
+             *
+             * This should be set to a class that extends \MusahMusah\LaravelMultipaymentGateways\Jobs\ProcessPaymentWebhookJob.
+             */
+            'payment_webhook_job' => '',
+
+            /**
+             * The payment_webhook_event option allows you to specify the event class that will be used to process webhook requests for payment methods.
+             *
+             * This should be set to a class that extends \MusahMusah\LaravelMultipaymentGateways\Events\PaymentWebhookReceivedEvent.
+             */
+            'payment_webhook_event' => '',
+        ],
+
+        [
+            /*
+             * This refers to the name of the payment gateway being used.
+             */
+            'name' => 'flutterwave',
+
+            /*
+             * This secret key is used to validate the signature of the webhook call.
+             */
+            'signing_secret' => '',
+
+            /*
+             * This refers to the header that holds the signature.
+             */
+            'signature_header_name' => 'Flutterwave-Signature',
+
+            /*
+             *  This class is responsible for verifying the validity of the signature header.
+             *
+            * It should implement the interface \MusahMusah\LaravelMultipaymentGateways\SignatureValidator\PaymentWebhookSignatureValidator.
+             */
+            'signature_validator' => \MusahMusah\LaravelMultipaymentGateways\SignatureValidator\DefaultSignatureValidator::class,
+
+            /**
+             * The webhook handler option allows you to choose how webhook requests are handled in your application.
+             *
+             * Available options:
+             * - 'job': Webhook requests will be handled by a job.
+             * - 'event': Webhook requests will be handled by an event.
+             *
+             * Default: 'job'
+             */
+            'payment_webhook_handler' => 'job',
 
             /**
              * The payment_webhook_job option allows you to specify the job class that will be used to process webhook requests for payment methods.
