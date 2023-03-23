2c02ee9a097fb3f82093a597fd5ae37495105238
cybernerdie <cybernerdie@users.noreply.github.com>
2023-03-23 09:24:57 +0000

    Fix styling

--- src/SignatureValidator/FlutterwaveSignatureValidator.php
+++ src/SignatureValidator/FlutterwaveSignatureValidator.php
@@ -3,21 +3,24 @@
 namespace MusahMusah\LaravelMultipaymentGateways\SignatureValidator;
 
 use Illuminate\Http\Request;
-use MusahMusah\LaravelMultipaymentGateways\Services\PaymentWebhookConfig;
 use Illuminate\Support\Facades\Log;
+use MusahMusah\LaravelMultipaymentGateways\Services\PaymentWebhookConfig;
 
 class FlutterwaveSignatureValidator implements PaymentWebhookSignatureValidator
 {
     public function isValid(Request $request, PaymentWebhookConfig $config): bool
     {
-        if ((! $request->isMethod('post')) || ! $request->hasHeader($config->signatureHeaderName)) return false;
+        if ((! $request->isMethod('post')) || ! $request->hasHeader($config->signatureHeaderName)) {
+            return false;
+        }
 
         $signature = $request->header($config->signatureHeaderName);
         $requestContent = $request->getContent();
         $signingSecret = $config->signingSecret;
 
         if (empty($signingSecret)) {
-            Log::error("The `FLUTTERWAVE_SECRET_HASH` is missing or not set in your env.");
+            Log::error('The `FLUTTERWAVE_SECRET_HASH` is missing or not set in your env.');
+
             return false;
         }
 
