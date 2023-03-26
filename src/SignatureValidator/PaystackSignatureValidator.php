ad62f8a41fa5acbde8b5bf89b9a2decb7df6fb14
MusahMusah <shehialiyu0@gmail.com>
2023-03-26 09:29:18 +0100

    added default signature validator for stripe and paystack

--- src/SignatureValidator/PaystackSignatureValidator.php
+++ src/SignatureValidator/PaystackSignatureValidator.php
@@ -9,6 +9,13 @@ class PaystackSignatureValidator implements PaymentWebhookSignatureValidator
 {
     public function isValid(Request $request, PaymentWebhookConfig $config): bool
     {
-        // TODO: Implement isValid() method.
+        if ((! $request->isMethod(method: 'post')) || ! $request->header(key: $config->signatureHeaderName)) return false;
+
+        $requestContent = $request->getContent();
+        $signature = hash_hmac(algo: 'sha512', data: $requestContent, key: $config->signingSecret);
+
+        if ($signature !== $request->header(key: $config->signatureHeaderName)) return false;
+
+        return hash_equals(known_string: $signature, user_string: $request->header($config->signatureHeaderName));
     }
 }
