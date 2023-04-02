da9c16324eb56c92f81d4fea58c6cb39ea405dd3
MusahMusah <shehialiyu0@gmail.com>
2023-04-02 04:18:47 +0100

    added flutterwave helper

--- src/Utils/Helpers.php
+++ src/Utils/Helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use MusahMusah\LaravelMultipaymentGateways\Contracts\FlutterwaveContract;
 use MusahMusah\LaravelMultipaymentGateways\Contracts\PaystackContract;
 use MusahMusah\LaravelMultipaymentGateways\Contracts\StripeContract;
 
@@ -14,5 +15,12 @@ if (! function_exists('stripe')) {
     function stripe(): StripeContract
     {
         return app(StripeContract::class);
+    }
+}
+
+if (! function_exists('flutterwave')) {
+    function flutterwave(): FlutterwaveContract
+    {
+        return app(FlutterwaveContract::class);
     }
 }
